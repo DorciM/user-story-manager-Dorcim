903715f687eb7a7f02d6617798a98528582db432
DorciM <dora.medgyasszay@gmail.com>
2016-09-18 23:07:28 +0200

    saving is not working

--- server.py
+++ server.py
@@ -4,20 +4,18 @@ from models import *
 app = Flask(__name__)
 
 
-# Index page and List page
-@app.route("/")
-@app.route("/list")
+@app.route("/", methods=['GET'])
+@app.route("/list", methods=['POST'])
 def list_data():
     query = User_story.select()
-    return render_template('list.html', user_stories=query, web2='http://127.0.0.1:5000/delete',
-                           web='http://127.0.0.1:5000/story')
+    return render_template('list.html', user_stories=query)
 
 
 # Story page, default
 @app.route("/story", methods=['GET'])
 def story():
-    empty_story = User_story(story_title="", content="", acceptance_criteria="", business_value="",
-                             estimation="", status="")
+    empty_story = User_story(story_title="", content="", acceptance_criteria="", business_value="100",
+                             estimation="0.5", status="")
     return render_template("form.html", user_story=empty_story)
 
 
@@ -30,36 +28,30 @@ def add_story():
                       business_value=request.form['business_value'],
                       estimation=request.form['estimation'],
                       status=request.form['status'])
-    return "Database uploaded!"
-
+    return redirect(url_for('list_data'), code=302)
 
 # Update a story by story_id
-@app.route("/story/<story_id>", methods=['POST'])
+@app.route("/story/<story_id>", methods=['POST','GET'])
 def update_story(story_id):
-    data = User_story.get(User_story.id == story_id)
-    q = data.update(story_title=request.form['story_title'],
-                    content=request.form['content'],
-                    acceptance_criteria=request.form['acceptance_criteria'],
-                    business_value=request.form['business_value'],
-                    estimation=request.form['estimation'],
-                    status=request.form['status'])
-    q.execute()
-    return "Story updated!"
+    if request.method == 'POST':
+        User_story.update(story_title=request.form['story_title'],
+                                 content=request.form['content'],
+                                 acceptance_criteria=request.form['acceptance_criteria'],
+                                 business_value=request.form['business_value'],
+                                 estimation=request.form['estimation'],
+                                 status=request.form['status']).where(User_story.id == story_id).execute()
+        return redirect(url_for('list_data'))
+    else:
+        user_story = User_story.get(User_story.id == story_id)
+        return render_template('form.html', user_story=user_story)
 
-
-# View a story, by story_id
-@app.route("/story/<story_id>", methods=['GET'])
-def show_story(story_id):
-    data = User_story.get(User_story.id == story_id)
-    return render_template('form.html', user_story=data)
 
 
 # Delete a story, by story_id
-@app.route("/delete/<story_id>", methods=['GET'])
+@app.route("/delete/<story_id>/", methods=['GET'])
 def delete_story(story_id):
-    data = User_story.get(User_story.id == story_id)
-    data.delete_instance()
-    return redirect('http://127.0.0.1:5000/list')
+    User_story.delete().where(User_story.id == story_id).execute()
+    return redirect(url_for('list_data'))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
